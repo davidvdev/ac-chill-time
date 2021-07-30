664364f05ba9f33b9dc189c36f87dfe610567799
davidvdev <dvsummercamp@gmail.com>
2021-07-29 22:31:58 -0500

    add hourly music.

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -42,11 +42,31 @@ const Clock =(props)=> {
     }
 
     const hourlyMusic = () => {
-        switch(timeArr[2]){
-            case 0:
-                return 'https://vgmsite.com/soundtracks/animal-crossing-gc-rip/ylidljqw/57%2012AM.mp3'
-            case 30:
-                return 'https://vgmsite.com/soundtracks/animal-crossing-gc-rip/lnudvooq/63%206AM.mp3'
+        switch(timeArr[0]){
+            case 0: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/ylidljqw/57%2012AM.mp3` //12AM
+            case 1: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/okeaglbw/58%201AM.mp3` //1AM
+            case 2: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/cjenocyk/59%202AM.mp3` //2AM
+            case 3: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/fuufxfdm/60%203AM.mp3` //3AM
+            case 4: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/fgzmwhir/61%204AM.mp3` //4AM
+            case 5: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/covridlr/62%205AM.mp3` //5AM
+            case 6: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/lnudvooq/63%206AM.mp3` //6AM
+            case 7: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/epeiucnl/64%207AM.mp3` //7AM
+            case 8: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/oycoafpr/65%208AM.mp3` //8AM
+            case 9: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/nnzvlwzu/66%209AM.mp3` //9AM
+            case 10: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/wdkwshgl/67%2010AM.mp3` //10AM
+            case 11: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/dxfktyaa/68%2011AM.mp3` //11AM
+            case 12: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/erkgvhgi/69%2012PM.mp3` //12PM
+            case 13: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/vkdqutcz/70%201PM.mp3` //1PM
+            case 14: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/rhxneszf/71%202PM.mp3` //2PM
+            case 15: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/lvamjoma/72%203PM.mp3` //3PM
+            case 16: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/svaplsve/73%204PM.mp3` //4PM
+            case 17: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/ganqlufi/74%205PM.mp3` //5PM
+            case 18: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/jlpodkzo/75%206PM.mp3` //6PM
+            case 19: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/azzmbawn/76%207PM.mp3` //7PM
+            case 20: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/xszkmlbp/77%208PM.mp3` //8PM
+            case 21: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/xjzxouqy/78%209PM.mp3` //9PM
+            case 22: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/zhcddudp/79%2010PM.mp3` //10PM
+            case 23: return `https://vgmsite.com/soundtracks/animal-crossing-gc-rip/nxlyyemi/80%2011PM.mp3` //11PM
         }
     }
 
